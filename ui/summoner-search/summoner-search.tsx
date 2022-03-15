3b902ec33e3d22a5dfa5a0ca95919f8feabfcaf8
Nils <58339645+7rebux@users.noreply.github.com>
2022-03-15 10:06:22 +0100

    Use css modules

--- ui/summoner-search/summoner-search.tsx
+++ ui/summoner-search/summoner-search.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { PlatformSelector, Platform } from '@league_ml/component-library.ui.platform-selector';
 import { FiSearch } from 'react-icons/fi';
 
-import './summoner-search.css'
+import styles from './summoner-search.module.css'
 
 export type SummonerSearchProps = {
   /** Specifies the initial platform for the platform selector */
@@ -14,10 +14,10 @@ export type SummonerSearchProps = {
 
 export function SummonerSearch({ initialPlatform = 'NA', onSearch }: SummonerSearchProps) {
   return (
-    <div className='summoner-search'>
+    <div className={ styles.summonerSearch }>
       <PlatformSelector initialPlatform={ initialPlatform }/>
-      <input placeholder='Type a summoner name' />
-      <div className='button' onClick={ onSearch }>
+      <input className={ styles.input } placeholder='Type a summoner name' />
+      <div className={ styles.button } onClick={ onSearch }>
         <FiSearch size='40' />
       </div>
     </div>
