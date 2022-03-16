20f6977de8950403733fc1389275e5c61ae2c40e
Nils <58339645+7rebux@users.noreply.github.com>
2022-03-16 12:38:18 +0100

    Update summoner search

--- src/components/SummonerSearch/SummonerSearch.tsx
+++ src/components/SummonerSearch/SummonerSearch.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { SummonerSearchProps } from './SummonerSearch.types';
+import { SummonerSearchProps, Platform } from './SummonerSearch.types';
 import './SummonerSearch.css';
 
 import PlatformSelector from '../PlatformSelector';
 import { FiSearch } from 'react-icons/fi';
 
 function SummonerSearch({ initialPlatform = 'NA', onSearch }: SummonerSearchProps) {
+  const [input, setInput] = useState<string>('');
+  const [platform, setPlatform] = useState<Platform>(initialPlatform)
+  
+  function handleSubmit() {
+    if (input !== '')
+      onSearch(input, platform)
+  }
+
+  function handleKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Enter')
+      handleSubmit()
+  }
+  
   return (
     <div className='summonerSearch'>
-      <PlatformSelector initialPlatform={ initialPlatform }/>
-      <input className='nameInput' placeholder='Type a summoner name' />
-      <div className='searchButton' onClick={ onSearch }>
+      <PlatformSelector 
+        initialPlatform={ platform }
+        onChange={ value => setPlatform(value) }
+      />
+      <input 
+        value={ input } 
+        onInput={ e => setInput(e.target.value) }
+        onKeyDown={ e => handleKeyDown(e) }
+        className='nameInput'
+        placeholder='Type a summoner name'
+      />
+      <div 
+        className='searchButton'
+        onClick={ () => handleSubmit() }
+       >
         <FiSearch size='40' />
       </div>
     </div>
